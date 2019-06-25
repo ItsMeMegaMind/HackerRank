a42c3565e99258a814a2df2d683f2f1983332160
Sandip Gautam <sandipgautam961@gmail.com>
2019-06-25 14:31:16 -0500

    HackerRank Solution for Kaprekar Number

---
+++ Algorithm/Implementation/Beautiful Triplets.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+from itertools import permutations
+# Complete the beautifulTriplets function below.
+def beautifulTriplets(d, arr):
+    perm = permutations(range(len(arr)),3)
+    out = 0
+    indices = []
+    for i in perm:
+        if(i[0]<i[1] and i[1]<i[2]):
+            indices.append(i)
+    for _ in indices:
+        i=_[0]
+        j=_[1]
+        k=_[2]
+        if(arr[j]-arr[i]==d and arr[k] - arr[j] ==d):
+            out+=1
+    return out
+if __name__ == '__main__':
+    print(1)
+    nd = input().split()
+
+    n = int(nd[0])
+
+    d = int(nd[1])
+    arr = list(map(int, input().rstrip().split()))
+
+    result = beautifulTriplets(d, arr)
+
+    print(result)
+
