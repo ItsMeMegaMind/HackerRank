c358f16c6aaa125bf955c51ab065b0876b844841
Sandip Gautam <sandipgautam961@gmail.com>
2019-06-25 10:55:26 -0500

    HackerRank Solution for Queen-Attack-2

--- Algorithm/Implementation/encryption.py
+++ Algorithm/Implementation/encryption.py
@@ -6,16 +6,20 @@ def encryption(s):
     n = len(s)
     l = math.sqrt(n)
     r,c = math.ceil(l),math.floor(l)
+    print(r,c)
     if(r*c<n): c+=1
     arr = [list(s[i:i+r]) for i in range(0,len(s),r)]
+    print(arr)
     out = ""
-    try:
-        for i in range(r):
+    for i in range(r):
+        try:
             for j in range(c):
                 out += (arr[j][i])
             out+=" "
-    except IndexError:
-            return(out)
+        except IndexError:
+            out+=" "
+            pass
+    return(out)
 s = input()
 print(encryption(s))
 
