0df883f5609694af2655c6bf59eb2af3bb175537
Sandip Gautam <sandipgautam961@gmail.com>
2019-06-25 14:38:24 -0500

    HackerRank Solution for Beautiful Triplets

--- Algorithm/Implementation/Beautiful Triplets.py
+++ Algorithm/Implementation/Beautiful Triplets.py
@@ -7,17 +7,9 @@ import sys
 from itertools import permutations
 # Complete the beautifulTriplets function below.
 def beautifulTriplets(d, arr):
-    perm = permutations(range(len(arr)),3)
     out = 0
-    indices = []
-    for i in perm:
-        if(i[0]<i[1] and i[1]<i[2]):
-            indices.append(i)
-    for _ in indices:
-        i=_[0]
-        j=_[1]
-        k=_[2]
-        if(arr[j]-arr[i]==d and arr[k] - arr[j] ==d):
+    for i in arr:
+        if(((i+d) in arr) and ((i+ 2*d) in arr)):
             out+=1
     return out
 if __name__ == '__main__':
@@ -31,4 +23,3 @@ if __name__ == '__main__':
     result = beautifulTriplets(d, arr)
 
     print(result)
-
