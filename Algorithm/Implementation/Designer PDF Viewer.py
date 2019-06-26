a722c732107e6f4aaf9f24a84771391d73bb1ad6
Sandip Gautam <sandipgautam961@gmail.com>
2019-06-26 09:22:53 -0500

    HackerRank Solution for Designer PDF Viewer

---
+++ Algorithm/Implementation/Designer PDF Viewer.py
@@ -0,0 +1,12 @@
+def designerPdfViewer(h, word):
+    return max([h[ord(i)-96] for i in word])*len(word)
+
+if __name__ == '__main__':
+
+    h = list(map(int, input().rstrip().split()))
+
+    word = input()
+
+    result = designerPdfViewer(h, word)
+
+    print(result)
