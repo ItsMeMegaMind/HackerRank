fec87226a59ef5687bd27876f477e8ad2d449ced
Sandip Gautam <sandipgautam961@gmail.com>
2019-07-08 11:41:53 -0500

    added solutions

---
+++ Algorithm/Implementation/Weighted Uniform String/Weighted Uniform String.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+def getWeight(a):
+    return ord(a)- 96
+
+if __name__ == '__main__':
+    s = input()
+   # out= []
+    out = set()
+    prv = ''
+    weight = 0
+    for curr in s:
+        if curr != prv:
+            weight = 0
+        weight += getWeight(curr)
+        out.add(weight)
+        prv=curr
+    for i in range(int(input())):
+        print("Yes" if int(input()) in out else "No")
+
+
