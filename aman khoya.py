42a4a295022a61c1e7fd2d7142dec5a873f380c4
Sandip Gautam <sandipgautam961@gmail.com>
2019-07-15 15:50:39 -0500

    added solutions

--- aman khoya.py
+++
@@ -1,6 +0,0 @@
-if __name__ == '__main__':
-    string, k = input(), int(input())
-    loop = len(string) // k
-    for i in range(0, len(string), k):
-        a = string[i:i + k]
-        print("".join(dict(zip(a, a)).keys()))
